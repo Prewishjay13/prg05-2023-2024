50c69c993ffcb0077559fd3bd92a9acf08ba1c23
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2023-10-08 23:18:36 +0200

    Update web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('test');
 });
+// Auth::routes();
+
+// Route::get('/posts', [App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
+// Route::get('/', [App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+
+// Route::get('/profile/{user}/edit',  [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
+// Route::get('/profile/{user}/{post}/edit',  [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
+// Route::post('/post', [App\Http\Controllers\PostsController::class, 'store'])->name('posts.store');
+// Route::get('/create', [App\Http\Controllers\PostsController::class, 'create'])->name('posts.create');
